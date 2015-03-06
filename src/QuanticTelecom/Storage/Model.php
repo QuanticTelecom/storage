d99a42440e647c02b818507f8d85ff28bc9583fe
Thibaud <thibaud@dauce.fr>
2015-03-06 15:07:17 +0100

    feat: business logic classes storage
    Add getter and setter for businessLogicClasses

--- src/QuanticTelecom/Storage/Model.php
+++ src/QuanticTelecom/Storage/Model.php
@@ -6,4 +6,23 @@ use Jenssegers\Mongodb\Model as Moloquent;
 class Model extends Moloquent
 {
     use SoftDeletingTrait;
+
+    /**
+     * @return string[] All the business logic classes the model come from.
+     */
+    public function getAllBusinessLogicClasses()
+    {
+        return $this->getAttribute('businessLogicClasses');
+    }
+
+    /**
+     * Set the business logic classes.
+     *
+     * @param string[] $businessLogicClasses
+     * @return void
+     */
+    public function setAllBusinessLogicClasses(array $businessLogicClasses)
+    {
+        $this->setAttribute('businessLogicClasses', $businessLogicClasses);
+    }
 }
